a849d2b27137ce34789bf23f7b2454a5e5b2796a
sreepathi-ravikumar <rksreepathi1@gmail.com>
2025-07-13 13:52:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,8 @@
   const videoLinks = [
     "video(1).mp4",
     "video(2).mp4",
-    "video(3).mp4"
+    "video(3).mp4",
+    "https://youtu.be/kx-cZjnz9pY?si=7NVg4SixkDJgU7Hq"
   ];
 
   // Step 2: Pick a random video from the array
