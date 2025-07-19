6b653a1deff7795dbd04654fb7f2778b25e51440
sreepathi-ravikumar <rksreepathi1@gmail.com>
2025-07-19 14:50:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -115,7 +115,7 @@
       const video = document.getElementById("myVideo");
       const source = document.getElementById("videoSource");
 
-      source.src = "https://cdn.pixabay.com/video/2021/05/27/75439-556022158_large.mp4";
+      source.src = "loading.mp4";
       video.removeAttribute("controls");
       video.load();
 
